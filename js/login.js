6140e087b340920f7da12205fe766d9380e3d671
Insoo <jincupia@gmail.com>
2022-12-26 20:13:44 +0900

    로그인 동작 수정

--- js/login.js
+++ js/login.js
@@ -14,14 +14,25 @@ iconVisiEl.addEventListener('click', () => {
   }
 });
 
-const chkEmail = /[^A-Za-z0-9@._-]/g;
-// const chkEmail = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
-const chkPW = /[^A-Za-z0-9!@#$%^&*()]/g;
 
-function inputEmail(e) {
-  e.value = e.value.replace(chkEmail, '');
-}
+// log in
+const emailStr = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+const passwordStr = /[A-Za-z0-9!@#$%^&*()]{12}/g;
 
-function inputPassword(e) {
-  e.value = e.value.replace(chkPW, '');
-}
+const btnLoginEl = document.querySelector('.btn--login');
+const emailInput = document.querySelector('input#e-mail');
+const passwordInput = document.querySelector('input#password');
+
+btnLoginEl.addEventListener('click', () => {
+  if (emailStr.test(emailInput.value)) {
+    if (passwordStr.test(passwordInput.value)) {
+      window.alert('로그인 성공');
+    } else {
+      window.alert('로그인에 실패하였습니다.\n비밀번호는 영문/숫자/특수기호 포함 12자 이상이어야 합니다.\n특수기호는 !@#$%^&* 만 사용 가능합니다.');
+    }
+  } else {
+    window.alert('올바른 이메일 형식을 입력 해 주세요.');
+  }
+  emailInput.value = '';
+  passwordInput.value = '';
+})
