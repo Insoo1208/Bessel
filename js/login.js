2e6a2eb7f786d02b36fe2dc0c0bda2eeb651cba4
Insoo <jincupia@gmail.com>
2022-12-26 18:05:19 +0900

    주석 수정

--- js/login.js
+++ js/login.js
@@ -15,6 +15,7 @@ iconVisiEl.addEventListener('click', () => {
 });
 
 const chkEmail = /[^A-Za-z0-9@._-]/g;
+// const chkEmail = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
 const chkPW = /[^A-Za-z0-9!@#$%^&*()]/g;
 
 function inputEmail(e) {
